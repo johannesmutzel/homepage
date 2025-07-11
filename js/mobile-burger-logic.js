5ff4eb1a40046dd61d5ffcbaf17ab05f386be05e
Johannes <johannesmutzel2004@gmail.com>
2025-07-11 11:03:03 +0200

    update homepage

--- js/mobile-burger-logic.js
+++ js/mobile-burger-logic.js
@@ -1,7 +1,15 @@
 const toggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav ul.nav li a');
 
 toggle.addEventListener('click', () => {
   toggle.classList.toggle('open');
-  nav.classList.toggle('open'); // öffnet dein <nav> oder <ul>
+  nav.classList.toggle('open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    toggle.classList.remove('open'); // Kreuz zurück zu Burger
+    nav.classList.remove('open');    // Menü ausblenden
+  });
 });
